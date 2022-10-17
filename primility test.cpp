ec60e59c65de9a953b32c77b99533598b093b35a
Md Nasim Hossen <102505098+Nasim0008@users.noreply.github.com>
2022-10-17 17:47:30 +0600

    Check the number is prime or not

---
+++ primility test.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+bool prime(int n)
+{
+    if(n<2) return false;
+    if(n<=3) return true;
+    if(n%2==0) return false;
+    for(int i=3;i*i<=n;i+=2)
+    {
+        if(n%i==0) return false;
+    }
+    return true;
+}
+int main()
+{
+    int n;
+    cout<< "Enter the Number: ";
+    cin>>n;
+    if(prime(n))
+    {
+        cout<< "Prime Number"<<endl;
+    }
+    else
+    {
+        cout<< "Not Prime Number"<<endl;
+    }
+}
