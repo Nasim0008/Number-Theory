659afc7fa64abfef9d1df1453ec8dc522a06c7d5
Md Nasim Hossen <102505098+Nasim0008@users.noreply.github.com>
2022-12-22 01:01:44 +0600

    check the number is odd or even

---
+++ odd_even_condition.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+       int64_t n;
+        cin>>n;
+        if(n&1)
+        {
+            cout<<"Odd Number"<<endl;
+        }
+        else
+        {
+            cout<< "Even Number"<<endl;
+        }
+    //cout<<n/2 + n%2<<endl;
+    //cout << (n+1) / 2 << endl;
+}
