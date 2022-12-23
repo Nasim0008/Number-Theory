3b34dbf663713b8b2f8668e080e573bbc4d47603
Md Nasim Hossen <102505098+Nasim0008@users.noreply.github.com>
2022-12-24 02:28:07 +0600

    check the given number is 2 exponential or not

---
+++ 2_exponential.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+    cin>>n;
+    double a = log2(n);
+    int u=a;
+    if(u==a)
+    {
+        cout<< "YES"<<endl;
+    }
+    else
+    {
+        cout<< "NO"<<endl;
+    }
+}
