f1700114fab9b035eba37a242de7d3a0e7eb09ec
Md Nasim Hossen <102505098+Nasim0008@users.noreply.github.com>
2023-01-07 11:53:00 +0600

    print all prime number from 1 to N by using seive algorithm

---
+++ Prime Number/Seive_algortim.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+using namespace std;
+void seive(int n)
+{
+    bool prime[n+3];
+    for(int i=2;i*i<=n;i++)
+    {
+        if(prime[i]==false)
+        {
+            for(int j=i*i;j<=n;j+=i)
+            {
+                prime[j]=true;
+            }
+        }
+    }
+    for(int i=2;i<=n;i++)
+    {
+        if(prime[i]==false)
+        {
+          cout<<i<< " ";
+        }
+    }
+    cout<<endl;
+    
+}
+int main()
+{
+    int n;
+    cin>>n;
+    seive(n);
+}
