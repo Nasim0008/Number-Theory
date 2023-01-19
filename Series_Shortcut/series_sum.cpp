5530a3ef3e6f718843e1f5a8d259a1da5e5553c7
Md Nasim Hossen <102505098+Nasim0008@users.noreply.github.com>
2023-01-19 12:50:41 +0600

    find the sum of  the binary exponential series

--- Series_Shortcut/series_sum.cpp
+++ Series_Shortcut/series_sum.cpp
@@ -1,1 +1,9 @@
-
+// find the value of (2^0 + 2^1 + 2^2 + ....... + 2^power);
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int power;
+    cin>>power;
+    cout<< pow(2,power+1)-1<<endl;
+}
