d4e71b3e9feb3646c1281d3416a95d2a47125fc8
Md Nasim Hossen <102505098+Nasim0008@users.noreply.github.com>
2023-08-04 12:07:36 +0600

    prefix gcd of the array

---
+++ prefix_gcd_of_the_array.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+    cin >> n;
+    int arr[n + 4];
+    for (int i = 1; i <= n; i++)
+    {
+        cin >> arr[i];
+    }
+    int gcd = 0;
+    vector<int> v;
+    v.push_back(gcd);
+    for (int i = 1; i <= n; i++)
+    {
+        gcd = __gcd(arr[i], gcd);
+        v.push_back(gcd);
+    }
+    cout<< v.size()<<endl;
+    for (auto u : v)
+    {
+        cout << u << " ";
+    }
+    cout << endl;
+}
